643894f4d9e2db5181da89a548480701ce202358
yhoo0094 <69442322+yhoo0094@users.noreply.github.com>
2024-04-17 20:33:17 +0900

    개발중

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ $(()=>{
 	setData();
 })
 
+//데이터 세팅
 function setData() {
 	let totalTag = '';
 	for(let item of data){
 		let tag = '<div class="itemCard">'
-					+ '<div class="itemImgBox">'
+					+ '<div class="itemImgBox itemInfo">'
 						+ '<img src="./images/' + item.IMG + '" class="itemImg">'
 					+ '</div>'
-					+ '<p class="itemName">' + item.ITM_NM + '(' + item.UNIT + '개)</p>'
-					+ '<p class="itemPrice">' + item.PRICE * item.UNIT + '원</p>'
+					+ '<div class="itemName itemInfo">' + item.ITM_NM + '(' + item.UNIT + '개)</div>'
+					+ '<div class="itemPrice itemInfo">' + item.PRICE * item.UNIT + '원</div>'
+					+ '<div class="itemBtn itemInfo"><button type="button" onClick="copyNm(this)" data-nm="' + item.ITM_NM + '">상품명 복사</button></div>'
 				+ '</div>';
 		totalTag += tag;		
 	}
 	$('#itemBox').html(totalTag);
+}
+
+//복사 이벤트
+function copyNm(item){
+	navigator.clipboard.writeText($(item).data('nm'));
 }
